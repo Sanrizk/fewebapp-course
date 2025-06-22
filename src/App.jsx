83fa07d52b57640ec0fae4fa028f2be2f33aa858
sanrizk <ikhsanrizki3@gmail.com>
2025-06-22 21:24:00 +0700

    Template Dashboard from Horizon UI with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Input from "./Input";
-
-function Home() {
-  return <h1>Ini Halaman Home di App.jsx</h1>;
-}
-
-export default function App() {
+import RtlLayout from "layouts/rtl";
+import AdminLayout from "layouts/admin";
+import AuthLayout from "layouts/auth";
+const App = () => {
   return (
-    <div>
-      <Router>
-        <nav>
-          <Link to="/">Beranda</Link>
-          <Link to="/input">Input Data</Link>
-        </nav>
+    <Routes>
+      <Route path="auth/*" element={<AuthLayout />} />
+      <Route path="admin/*" element={<AdminLayout />} />
+      <Route path="rtl/*" element={<RtlLayout />} />
+      <Route path="/" element={<Navigate to="/admin" replace />} />
+    </Routes>
+  );
+};
 
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/input" element={<Input />} />
-        </Routes>
-      </Router>    
-    </div>
-  )
-}
+export default App;
